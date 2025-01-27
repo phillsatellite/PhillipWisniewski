0a089c3cc3e3766a239c646425e74ed3f365c9e3
Phill Wisniewski <phillwisniewski@macbookpro.lan>
2025-01-27 06:38:05 -0500

    Adding more test code

--- cypress/e2e/homePage.cy.js
+++ cypress/e2e/homePage.cy.js
@@ -5,21 +5,39 @@ describe(`Home Page Tests`, () => {
     cy.visit(`https://phillsatellite.github.io/PhillipWisniewski/PhillipWisniewski_Resume#`);
   });
 
+    //These tests focus on everything on the landing page
     it(`should display the name Phillip Wisniewski`, () => {
       cy.get(`h1`)
       .should(`be.visible`)
       .and(`contain`, `Phillip Wisniewski`);
     })
 
-    it.skip(`should display the job title and location`, () => {
-      cy.get(`[data-cy="info"]`)
+    it(`should display the job title and location`, () => {
+      cy.get(`[data-cy=info]`)
       .should(`be.visible`)
       .and(`contain`, `QA Engineer | Hickory, North Carolina`);
     })
 
+    it(`validates picture appears at top`, () => {
+      cy.get(`.profile-pic`)
+      .should(`be.visible`);
+    })
+
+    it(`Validates Contact Me button appears`, () => {
+      cy.get('[onclick="openModal()"]')
+      .should(`be.visible`)
+      .and(`contain.text`, `Contact Me`);
+    })
+
+    //These tests focus on the Contact Me modal on the landing page
     it(`Should open and verify contact modal`, () => {
       cy.openModal()
       .should(`be.visible`)
-      .and(`contain`, `Let's talk!`);
+      .and(`contain.text`, `Let's talk!`);
+    })
+
+    it(`All 3 buttons appear`, () => {
+      cy.openModal()
+      cy.get(`#emailButton`).should(`be.visible`).and(`contain.text`, `Email Me`);
     })
   })
