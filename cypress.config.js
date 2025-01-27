8a06a8d217b526b26c0e6de0def5b2f7eb89cc33
Phill Wisniewski <phillwisniewski@macbookpro.lan>
2025-01-27 05:30:29 -0500

    config fix

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://phillsatellite.github.io/PhillipWisniewski/PhillipWisniewski_Resume#',
+    setupNodeEvents(on, config) {
+      // Add any event listeners here, if needed
+    },
+  },
+});
